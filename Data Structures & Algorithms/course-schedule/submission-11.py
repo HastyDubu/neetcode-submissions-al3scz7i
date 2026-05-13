c2f0cbd59deca85170d4f02c555067a53f169bac
HastyDubu <gandresemmanuel@gmail.com>
2026-05-13 15:41:00 -0600

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = defaultdict(list)
+        for src, pre in prerequisites:
+            courses[src].append(pre)
+        
+        visit = set()
+        def dfs(src):
+            if src in visit:
+                return False
+            if not courses[src]:
+                return True
+            
+            visit.add(src)
+            for nei in courses[src]:
+                if not dfs(nei):
+                    return False
+            visit.remove(src)
+            courses[src] = []
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
