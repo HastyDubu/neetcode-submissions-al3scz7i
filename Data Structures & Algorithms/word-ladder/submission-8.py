454131b95b91f4c5382f0a33958759d0b3aadd2f
HastyDubu <gandresemmanuel@gmail.com>
2026-08-07 12:34:48 -0600

    Add: word-ladder - submission-8

---
+++ Data Structures & Algorithms/word-ladder/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+        
+        adj = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for i in range(len(word)):
+                w = word[:i] + "*" + word[i + 1:]
+                adj[w].append(word)
+        
+        q = deque([beginWord])
+        visit = set([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j + 1:]
+                    for nei in adj[pattern]:
+                        if nei not in visit:
+                            visit.add(nei)
+                            q.append(nei)
+            res += 1
+        return 0
